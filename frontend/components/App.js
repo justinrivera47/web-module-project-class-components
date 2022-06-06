0d5a0681acefed4cd9a3ad12bf7e6733a2932aef
justinrivera47 <justinrivera47@gmail.com>
2022-06-06 12:38:05 -0400

    I mapped over the todo state and created the form on the app component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,38 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  state = {
+    todo:[
+      {
+        name: 'Organize Garage',
+        id: 1528817077286, // could look different, you could use a timestamp to generate it
+        completed: false
+      },
+      {
+        name: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+      }
+    ]
+  }
+
+
   render() {
     return (
       <div>
-        Todo App
+        <ul>
+          {
+            this.state.todo.map(todo => {
+              return <li key={todo.id}>{todo.name}<span>{todo.completed}</span></li>
+              }
+            )
+          }
+        </ul>
+        <form>
+          <input />
+          <button>Submit</button>
+        </form>
+        <button>Clear Button</button>
       </div>
     )
   }
