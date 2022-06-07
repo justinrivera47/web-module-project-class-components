3667a0f19ba02318e019c98fb8957da77d2cb80a
justinrivera47 <justinrivera47@gmail.com>
2022-06-06 22:08:36 -0400

    toggle completed is wired from app to todo component

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -4,7 +4,7 @@ export default class Todo extends React.Component {
   render() {
     return (
       <div>
-        <li key={this.props.todo.id}>{this.props.todo.name}{this.props.todo.completed ? <span> ✔</span> : <span></span>}</li>
+        <li onClick={this.props.toggleComplete} key={this.props.todo.id}>{this.props.todo.name}{this.props.todo.completed ? <span> ✔</span> : <span></span>}</li>
       </div>
     )
   }
